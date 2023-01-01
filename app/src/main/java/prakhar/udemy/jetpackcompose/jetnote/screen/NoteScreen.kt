b962d80c6d1134bc6a691f29b618f1022eeb7f88
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-01-01 13:51:11 +0530

    Project Setup

---
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetnote/screen/NoteScreen.kt
@@ -0,0 +1,15 @@
+package prakhar.udemy.jetpackcompose.jetnote.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NoteScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NotesScreenPreview() {
+    NoteScreen()
+}
