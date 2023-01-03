21d682304ef9dfc69552c9cdca534cc2f8857f92
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-01-03 16:42:48 +0530

    Coroutines Suspend Functions

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetnote/data/NoteDatabaseDao.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetnote/data/NoteDatabaseDao.kt
@@ -10,17 +10,17 @@ interface NoteDatabaseDao {
     fun getNotes(): List<Note>
 
     @Query("SELECT * from notes_tbl where id =:id")
-    fun getNoteById(id: String): Note
+    suspend fun getNoteById(id: String): Note
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(note: Note)
+    suspend fun insert(note: Note)
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun update(note: Note)
+    suspend fun update(note: Note)
 
     @Query("DELETE  from notes_tbl")
-    fun deleteAll()
+    suspend fun deleteAll()
 
     @Delete
-    fun deleteNote(note: Note)
+    suspend fun deleteNote(note: Note)
 }
