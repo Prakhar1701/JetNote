2feb387bd47d9d4a6b84e4004289fd771fca2818
Prakhar Agrawal <prakharagrawal170101@outlook.com>
2023-01-03 17:12:23 +0530

    Added Providers in AppModule Class

--- app/src/main/java/prakhar/udemy/jetpackcompose/jetnote/di/AppModule.kt
+++ app/src/main/java/prakhar/udemy/jetpackcompose/jetnote/di/AppModule.kt
@@ -1,11 +1,32 @@
 package prakhar.udemy.jetpackcompose.jetnote.di
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import prakhar.udemy.jetpackcompose.jetnote.data.NoteDatabase
+import prakhar.udemy.jetpackcompose.jetnote.data.NoteDatabaseDao
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
 
+    @Singleton
+    @Provides
+    fun provideNotesDao(noteDatabase: NoteDatabase): NoteDatabaseDao = noteDatabase.noteDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): NoteDatabase =
+        Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            "notes_db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
 }
